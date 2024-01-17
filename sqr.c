f53a4cb1fc93f85037e6e29bcad66dfae1d749cf
mirii Alex Njuguna <145835776+Mirii100@users.noreply.github.com>
2024-01-17 08:28:17 +0300

    sqr.c

---
+++ sqr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int x=1,sqr;
+
+do
+{ 
+sqr=x*x;
+printf("%d*%d=%d\n",x,x,sqr);x++; 
+}
+while(x<=20);
+  int a=1,square;
+while(a<=20){square=a*a;
+printf("%d*%d=%d\n",a,a,square);
+a++;
+}
+return 0;
+}
