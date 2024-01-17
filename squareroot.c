ff44e694e858f5d4dcf11f7c5fbf70976a60fe7a
mirii Alex Njuguna <145835776+Mirii100@users.noreply.github.com>
2024-01-17 08:15:44 +0300

    squareroot.c

---
+++ squareroot.c
@@ -0,0 +1,12 @@
+//math function
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	float y,result;
+	printf("input a number\t");
+	scanf("%f",&y);
+	result=sqrt(y);
+	printf("sqrt=%f\n",result);
+	return 0;
+} 
